f27e9af8e94efdd02958169c27cf579f78d3bb9c
datodia <dato.diasamdize.02@gmail.com>
2023-03-25 22:32:43 +0400

    feat: add error page, header and modal

--- src/pages/Errorpage/Errorpage.jsx
+++ src/pages/Errorpage/Errorpage.jsx
@@ -1,7 +1,48 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 export const Errorpage = () => {
     return (
-        <div>Error</div>
+        <Container>
+            <TiTle>ოპაა...</TiTle>
+            <Desc>გვერდი ვერ მოიძებნა</Desc>
+            <Button >
+                <SLink to={'/'}>მთავარ გვერდზე დაბრუნება</SLink>
+            </Button>
+        </Container>
     )
 }
+
+
+const Container = styled.div`
+    width: 330px;
+    height: 100vh;
+    margin: auto;
+    display: flex; 
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+const TiTle = styled.h1`
+    font-size: 30px;
+    
+`
+const Desc = styled.h2`
+    font-size: 14px;
+    margin: 10px 0 30px 0;
+`
+const Button = styled.div`
+    width: 211px;
+    height: 50px;
+    background-color: var(--black);
+    border-radius: 25px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+const SLink = styled(Link)`
+    text-decoration: none;
+    font-size: 14px;
+    color: var(--white);
+`
